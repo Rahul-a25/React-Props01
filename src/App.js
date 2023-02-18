2ebba182f631b919fdeefad5f73c1f890a67c643
unknown <rahularora993676@gmail.com>
2023-02-18 13:08:04 -0800

    props01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from "./component/Card"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="first">
+       <Card
+       code="#FF6663"
+      para="Pink"
+      bgcolor="#FF6663"
+       />
+       <Card
+       code="#333333"
+       para="GRAY"
+       bgcolor="#333333"
+       />
+       <Card
+       code="#000000"
+       para="BLACK"
+       bgcolor="#000000"
+       />
+       <Card
+       code="#38BB14"
+       para="GREEN"
+       bgcolor="#38BB14"
+       />
+       <Card
+       code="##C90B0B"
+       para="RED"
+       bgcolor="#C90B0B"
+       />
+       </div>
+
+       <div className="second">
+       <Card
+       code="#FF8000"
+       para="ORANGE"
+       bgcolor="#FF8000"
+       />
+       <Card
+       code="#FFF500"
+       para="YELLOW"
+       bgcolor="#FFF500"
+       />
+       <Card
+       code="#CCCCCC"
+       para=" LIGHT GRAY"
+       bgcolor="#CCCCCC"
+       />
+       <Card
+       code="#7E41A2"
+       para="PURPLE"
+       bgcolor="#7E41A2"
+       />
+       <Card
+       code="#C14911"
+       para="BROWN"
+       bgcolor="#C14911"
+       />
+       </div>
     </div>
   );
 }
